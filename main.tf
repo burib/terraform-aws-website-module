49ca3a93a6755d188353fe0ab53c4bfecaaf8c8a
Balázs Buri <burib@users.noreply.github.com>
2024-11-15 11:31:32 +0100

    chore: remove environment variable [skip ci]

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@
 #   source = "github.com/burib/terraform-aws-ui-module?ref=v0"
 #
 #   domain_name              = "example.com"
-#   environment             = "prod"
 #   wildcard_certificate_arn = "arn:aws:acm:us-east-1:123456789012:certificate/xxx"
 #   route53_zone_id         = "ZXXXXXXXXXXXXX"
 #   redirect_www_to_https   = true  # Optional, defaults to true
@@ -20,11 +19,6 @@
 variable "domain_name" {
   type        = string
   description = "Domain name for the website"
-}
-
-variable "environment" {
-  type        = string
-  description = "Environment (dev/staging/prod)"
 }
 
 variable "wildcard_certificate_arn" {
